cd17d3fb5c05894e8eda96e5dc6e0a76c99bb894
jose.pereda <jose.pereda@gluonhq.com>
2024-08-07 19:21:41 +0200

    Use jSerialComm instead of Pi4J

--- ch12-RaspberryPi/Sample7/src/main/java/module-info.java
+++ ch12-RaspberryPi/Sample7/src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module org.modernclients.raspberrypi.gps {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.pi4j;
     requires com.gluonhq.maps;
     requires afterburner.mfx;
     requires java.annotation;
     requires java.logging;
+    requires com.fazecast.jSerialComm;
 
     opens org.modernclients.raspberrypi.gps.model to afterburner.mfx;
     opens org.modernclients.raspberrypi.gps.service to afterburner.mfx;
--- ch12-RaspberryPi/Sample7/src/main/java/org/modernclients/raspberrypi/gps/service/GPSService.java
+++ ch12-RaspberryPi/Sample7/src/main/java/org/modernclients/raspberrypi/gps/service/GPSService.java
@@ -1,12 +1,8 @@
 package org.modernclients.raspberrypi.gps.service;
 
-import com.pi4j.Pi4J;
-import com.pi4j.context.Context;
-import com.pi4j.io.serial.FlowControl;
-import com.pi4j.io.serial.Parity;
-import com.pi4j.io.serial.Serial;
-import com.pi4j.io.serial.StopBits;
-import com.pi4j.util.Console;
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -14,9 +10,7 @@ import org.modernclients.raspberrypi.gps.model.GPSPosition;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.logging.Logger;
 
 public class GPSService {
@@ -26,9 +20,9 @@ public class GPSService {
     @Inject
     private GPSPosition gpsPosition;
 
-    private Serial serial;
     private NMEAParser nmea;
     private StringBuilder gpsOutput;
+    private SerialPort comPort;
 
     private final StringProperty line = new SimpleStringProperty();
 
@@ -41,103 +35,31 @@ public class GPSService {
         nmea = new NMEAParser(gpsPosition);
         gpsOutput = new StringBuilder();
 
-        Console console = new Console();
-        Context context = Pi4J.newAutoContext();
+        comPort = getSerialPort("ttyAMA0");
+        if (comPort == null) {
+            throw new RuntimeException("comPort is null");
+        }
 
-        serial = context.create(Serial.newConfigBuilder(context)
-                .use_9600_N81()
-                .dataBits_8()
-                .parity(Parity.NONE)
-                .stopBits(StopBits._1)
-                .flowControl(FlowControl.NONE)
-                .id("my-serial")
-                .device("/dev/ttyAMA0")
-                .build());
-
-        try {
-            // Wait till the serial port is open
-            console.print("Waiting till serial port is open");
-            while (!serial.isOpen()) {
-                Thread.sleep(250);
+        comPort.openPort();
+        comPort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
             }
 
-            // Start a thread to handle the incoming data from the serial port
-            SerialReader serialReader = new SerialReader(console, serial);
-            Thread serialReaderThread = new Thread(serialReader, "SerialReader");
-            serialReaderThread.setDaemon(true);
-            serialReaderThread.start();
-
-            while (serial.isOpen()) {
-                Thread.sleep(500);
-            }
-
-            serialReader.stopReading();
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private class SerialReader implements Runnable {
-
-        private final Console console;
-        private final Serial serial;
-
-        private boolean continueReading = true;
-
-        public SerialReader(Console console, Serial serial) {
-            this.console = console;
-            this.serial = serial;
-        }
-
-        public void stopReading() {
-            continueReading = false;
-        }
-
-        @Override
-        public void run() {
-            // We use a buffered reader to handle the data received from the serial port
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(serial.getInputStream()))) {
-                try {
-                    // Data from the GPS is received in lines
-                    String line = "";
-
-                    // Read data until the flag is false
-                    while (continueReading) {
-                        // First we need to check if there is data available to read.
-                        // The read() command for pigio-serial is a NON-BLOCKING call,
-                        // in contrast to typical java input streams.
-                        var available = serial.available();
-                        if (available > 0) {
-                            for (int i = 0; i < available; i++) {
-                                byte b = (byte) br.read();
-                                if (b < 32) {
-                                    // All non-string bytes are handled as line breaks
-                                    if (!line.isEmpty()) {
-                                        // Here we should add code to parse the data to a GPS data object
-                                        console.println("Data: '" + line + "'");
-                                        gpsOutput.append(line);
-                                        line = "";
-                                        processReading();
-                                    }
-                                } else {
-                                    line += (char) b;
-                                }
-                            }
-                        } else {
-                            Thread.sleep(10);
-                        }
-                    }
-                } catch (Exception e) {
-                    console.println("Error reading data from serial: " + e.getMessage());
-                    e.getStackTrace();
+            @Override
+            public void serialEvent(SerialPortEvent event) {
+                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE) {
+                    return;
                 }
-            } catch (IOException e) {
-                console.println("Error reading data from serial: " + e.getMessage());
-                e.getStackTrace();
-                throw new RuntimeException(e);
+                byte[] newData = new byte[comPort.bytesAvailable()];
+                comPort.readBytes(newData, newData.length);
+                gpsOutput.append(new String(newData, Charset.defaultCharset())
+                        .replaceAll("\n", "")
+                        .replaceAll("\r", ""));
+                processReading();
             }
-        }
+        });
     }
 
     private void processReading() {
@@ -174,13 +96,24 @@ public class GPSService {
     }
 
     public void stop() {
-        logger.info("Stopping Serial and GPIO");
-        if (serial != null) {
+        logger.info("Stopping SerialPort");
+        if (comPort != null) {
             try {
-                serial.close();
+                comPort.removeDataListener();
+                comPort.closePort();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static SerialPort getSerialPort(String portName) {
+        SerialPort[] comPorts = SerialPort.getCommPorts();
+        for (SerialPort port : comPorts) {
+            if (portName.equals(port.getSystemPortName())) {
+                return port;
+            }
+        }
+        return null;
     }
 }
